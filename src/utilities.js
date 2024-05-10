3f9d4afb19d74ef64416180fe9a5b50dfd3239c1
valik <fedorovvalentin843@gmail.com>
2024-05-10 12:34:16 +0300

    commit

--- src/utilities.js
+++ src/utilities.js
@@ -1,9 +1,18 @@
 // Define our labelmap
-const labelMap = {
-    1:{name:'ThumbsUp', color:'red'},
-    2:{name:'ThumbsDown', color:'yellow'},
-    3:{name:'ThankYou', color:'lime'},
-    4:{name:'LiveLong', color:'blue'},
+ const labelMap = {
+    1:{name:'Student', color:'#7FFFD4'},
+    2:{name:'pyroh', color:'#FFE4C4'},
+    3:{name:'Good', color:'#0000FF'},
+    4:{name:'Ok', color:'#8A2BE2'},
+    5:{name:'Salute', color:'#D2691E'},
+    6:{name:'Fuck', color:'#5F9EA0'},
+    7:{name:'Bad', color:'#FF7F50'},
+    8:{name:'Uwu', color:'#FF1493'},
+    9:{name:'Freedom', color:'#DC143C'},
+    10:{name:'Victory', color:'#FFF8DC'},
+    11:{name:'Hi', color:'#8FBC8F'},
+    12:{name:'Heart', color:'#B22222'},
+    13:{name:'Please', color:'#008000'}
 }
 
 // Define a drawing function
@@ -13,13 +22,13 @@ export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight,
             // Extract variables
             const [y,x,height,width] = boxes[i]
             const text = classes[i]
-            
+
             // Set styling
             ctx.strokeStyle = labelMap[text]['color']
             ctx.lineWidth = 10
-            ctx.fillStyle = 'white'
-            ctx.font = '30px Arial'         
-            
+            ctx.fillStyle = 'black'
+            ctx.font = '30px Arial'
+
             // DRAW!!
             ctx.beginPath()
             ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
